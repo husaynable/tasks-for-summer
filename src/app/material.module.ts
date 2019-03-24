dd8ff6b4b40ce244f90599a8efe1bec2d1ef384d
Хусайн Эсамбаев <husainesambaev@gmail.com>
2019-03-24 21:06:22 +0300

    fixed menu

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,9 +4,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  imports: [MatCheckboxModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-  exports: [MatCheckboxModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
+  imports: [
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule
+  ],
+  exports: [
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule
+  ]
 })
 export class MaterialModule { }
