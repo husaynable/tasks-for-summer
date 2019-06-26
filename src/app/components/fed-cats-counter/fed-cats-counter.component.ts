39e296574293633f4658cc25fc4ce6a40d9cadfe
Хусайн Эсамбаев <husainesambaev@gmail.com>
2019-06-26 10:32:51 +0300

    added animations to counter buttons

--- src/app/components/fed-cats-counter/fed-cats-counter.component.ts
+++ src/app/components/fed-cats-counter/fed-cats-counter.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FED_CATS_COUNT } from 'src/app/services/fed-cats-counter-overlay.service';
 import { FedCatsCounterOverlayRef } from 'src/app/services/fed-cats-counter-overlay-ref';
+import { trigger, transition, animate, style, state } from '@angular/animations';
 
 @Component({
   selector: 'app-fed-cats-counter',
   templateUrl: './fed-cats-counter.component.html',
-  styleUrls: ['./fed-cats-counter.component.css']
+  styleUrls: ['./fed-cats-counter.component.css'],
+  animations: [
+    trigger('saveState', [
+      state('ready', style({ transform: 'translateX(20px)' })),
+      transition('* => ready', animate('325ms cubic-bezier(0, 0, 0.2, 1)'))
+    ]),
+    trigger('cancelState', [
+      state('ready', style({ transform: 'translateX(-20px)' })),
+      transition('* => ready', animate('325ms cubic-bezier(0, 0, 0.2, 1)'))
+    ]),
+    trigger('addState', [
+      state('ready', style({ transform: 'translateY(-20px)' })),
+      transition('* => ready', animate('325ms cubic-bezier(0, 0, 0.2, 1)'))
+    ]),
+    trigger('subtractState', [
+      state('ready', style({ transform: 'translateY(20px)' })),
+      transition('* => ready', animate('325ms cubic-bezier(0, 0, 0.2, 1)'))
+    ])
+  ]
 })
 export class FedCatsCounterComponent implements OnInit {
   catsCount: number;
